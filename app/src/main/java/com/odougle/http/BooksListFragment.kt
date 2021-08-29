397909c0c204d36af827a8a5562efc6e0b61a3ae
douguizilla <douglasgommez@gmail.com>
2021-08-29 18:34:51 -0300

    set view binding on BooksListFragment.kt

--- app/src/main/java/com/odougle/http/BooksListFragment.kt
+++ app/src/main/java/com/odougle/http/BooksListFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.odougle.http.databinding.FragmentBooksListBinding
+
 class BooksListFragment : Fragment() {
+
+    private var _binding: FragmentBooksListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        retainInstance = true
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books_list, container, false)
+        _binding = FragmentBooksListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
