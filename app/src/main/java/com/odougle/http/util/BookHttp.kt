3588b63fac29936eb4f45df05625f8e983593c01
douguizilla <douglasgommez@gmail.com>
2021-08-29 18:08:56 -0300

    implemented hasConnection and loadBooks methods

--- app/src/main/java/com/odougle/http/util/BookHttp.kt
+++ app/src/main/java/com/odougle/http/util/BookHttp.kt
@@ -1,5 +1,10 @@
 package com.odougle.http.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import com.odougle.http.model.Book
+import org.json.JSONObject
+import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -7,6 +12,7 @@ object BookHttp {
     val BOOK_JSON_URL = "https://raw.githubusercontent.com/nglauber/" +
             "dominando_android3/master/livros_novatec.json"
 
+    @Throws(IOException::class)
     private fun connect(urlAddress: String) : HttpURLConnection{
         val second = 1000
         val url = URL(urlAddress)
@@ -19,5 +25,26 @@ object BookHttp {
         }
         connection.connect()
         return connection
+    }
+
+    fun hasConnection(context: Context) : Boolean{
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val info = cm.activeNetworkInfo
+        return info != null && info.isConnected
+    }
+
+    fun loadBooks() : List<Book>?{
+        try {
+            val connection = connect(BOOK_JSON_URL)
+            val responseCode = connection.responseCode
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                val inputStream = connection.inputStream
+                val json = JSONObject(streamToString(inputStream))
+                return readBooksFromJson(json)
+            }
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+        return null
     }
 }
